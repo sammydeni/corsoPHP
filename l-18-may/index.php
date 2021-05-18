614aba676fe9dbbbbde7777365131b92de534a2a
Samuele Denicola <samueledenicola01@gmail.com>
2021-05-18 10:59:06 +0200

    up

---
+++ l-18-may/index.php
@@ -0,0 +1,16 @@
+<?php
+
+$lorem = file_get_contents("https://restcountries.eu/rest/v2/region/europe");
+
+// echo $lorem;
+
+$json = json_decode($lorem);
+
+echo "<ul>";
+foreach ($json as $key => $paese) {
+    echo "<li>".$paese->name."</li>";
+}
+echo "</ul>";
+
+// echo ($json[25]->name);
+?>
